d89f254409acbf76cc563398f217709d557c3139
Priyanka Rokhade <priyankavrokhade@gmail.com>
2024-11-11 20:34:29 +0530

    updated code for increment decrement

--- cart.js
+++ cart.js
@@ -3,18 +3,49 @@ let cart = JSON.parse(localStorage.getItem("cart")) || [];
 function displayCart() {
   const cartList = document.getElementById("cart-list");
   cartList.innerHTML = "";
+
+  if (cart.length === 0) {
+    cartList.innerHTML = "<p>Your cart is empty.</p>";
+  }
+
   cart.forEach((book, index) => {
     const cartItem = document.createElement("div");
     cartItem.className = "cart-item";
     cartItem.innerHTML = `
       <h3>${book.title}</h3>
       <p>Price: $${book.price}</p>
-      <p>Quantity: ${book.quantity}</p>
+      <div class="display">
+      <button class="btn" onClick="decrementQuantity(${index})">-</button>
+      <p>Quantity: ${book.quantity}</p>     
+      <button class="btn" onClick="incrementQuantity(${index})">+</button>
+      </div>
       <button onclick="removeFromCart(${index})">Remove</button>
     `;
     cartList.appendChild(cartItem);
   });
+
+  const clearCartButton = document.createElement("button");
+  clearCartButton.innerText = "Clear Cart";
+  clearCartButton.onclick = clearCart;
+  clearCartButton.classList.add("clear-cart-btn");
+  cartList.appendChild(clearCartButton);
   displaySubtotal();
+}
+
+function incrementQuantity(index) {
+  cart[index].quantity += 1;
+  localStorage.setItem("cart", JSON.stringify(cart));
+  displayCart();
+}
+
+function decrementQuantity(index) {
+  if (cart[index].quantity > 1) {
+    cart[index].quantity -= 1;
+  } else {
+    cart.splice(index, 1);
+  }
+  localStorage.setItem("cart", JSON.stringify(cart));
+  displayCart();
 }
 
 function removeFromCart(index) {
@@ -28,13 +59,19 @@ function removeFromCart(index) {
   displayCart();
 }
 
+window.onload = displayCart;
+
+function clearCart() {
+  cart = [];
+  localStorage.setItem("cart", JSON.stringify(cart));
+  displayCart();
+}
+
 function displaySubtotal() {
   const subtotal = cart.reduce(
     (total, book) => total + Number(book.price * book.quantity),
     0
   );
-  console.log("Calculated subtotal:", subtotal);
-
   const subtotalElement = document.getElementById("subtotal");
   if (subtotalElement) {
     subtotalElement.innerText = `Subtotal: $${subtotal.toFixed(2)}`;
